07b4c95203e3ad9e54e584972b4aef3f003eb1b3
Shismohammad <shismohammadmulla@gmail.com>
2023-02-28 20:53:47 +0530

    Added Navbar working

--- my_react/src/App.js
+++ my_react/src/App.js
@@ -6,14 +6,14 @@ import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    /*<div classNameName="App">
+      <header classNameName="App-header">
+        <img src={logo} classNameName="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          classNameName="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -24,7 +24,7 @@ function App() {
     </div>*/
     <>
       <Navbar />
-      <LoginForm />
+      {/* <LoginForm /> */}
     </>
   );
 }
